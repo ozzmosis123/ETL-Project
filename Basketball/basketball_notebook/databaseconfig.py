f46816c3c747140d1cde3bd1419cd225ae538e7b
Kazuki0131 <57879055+Kazuki0131@users.noreply.github.com>
2020-03-06 17:03:56 -0800

    Add files via upload

---
+++ Basketball/basketball_notebook/databaseconfig.py
@@ -0,0 +1,7 @@
+#Your PostgresSQL server information
+sql = {'host':'', #enter the host name
+        'port':'', #enter the port number
+        'user':'', #enter the user name
+        'password':'', #enter the password
+        'database':'' #enter the database name
+        }
